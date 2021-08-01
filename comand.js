d9a00b74c1165ddff85908edf78ba05f470fe029
Max-Rus1977 <maxsvet77@yandex.ru>
2021-08-01 14:50:58 +0300

    finish commit

--- comand.js
+++ comand.js
@@ -1,6 +1,8 @@
 const board = document.querySelector('#board');
 
-const SQUERS_NUMBER = 450;
+const colors = ['#d33e9e', '#8e44ad', '#74b8e5', '#d9ac83', '#2ecc71', '#cd1c1c'];
+
+const SQUERS_NUMBER = 600;
 
 for (let i = 0; i < SQUERS_NUMBER; i++) {
   const squaere = document.createElement('div');
@@ -13,9 +15,17 @@ for (let i = 0; i < SQUERS_NUMBER; i++) {
 }
 
 function setColor(element) {
-  element.style.backgroundColor = 'red';
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
 }
 
 function removeColor(element) {
   element.style.backgroundColor = '#1d1d1d';
+  element.style.boxShadow = '0 0 2px #1d1d1d';
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
 }
